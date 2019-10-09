2858f24fbf9a530511e06e4f8645cf37f0b7cf62
Cleopatra <piperschwartz@aol.com>
2019-10-08 19:41:43 -0600

    commit 2

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-var timeEl = document.querySelector(".Time")
+console.log("hello");
+
+var timeEl = document.querySelector(".Time");
 var questionsEl = document.querySelector(".questions");
 
 var secondsLeft = 75;
 var timerInterval;
 //var highScoreEl = document.querySelector("#scoring");
 
-function setTime() 
-{
-    timerInterval = setInterval(function() 
-    {
-        secondsLeft--;
-        timeEl.textContent = "Time: " + secondsLeft;
-    }, 1000);
-
+function setTime() {
+  timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+  }, 1000);
 }
 
-function setTime() 
+/*function setTime() 
 {
     timerInterval = setInterval (function ()
     {
@@ -27,65 +26,55 @@ function setTime()
         }
      finish ();
         })
-
-
+*/
 
 const startBtn = document.getElementById("start");
-startBtn.addEventListener('click', function()
-{
-    setTime();
-    startBtn.style.display = 'none';
+console.log(startBtn);
+startBtn.addEventListener("click", function() {
+  setTime();
+  startBtn.style.display = "none";
 
-    displayQuestions();
-})
+  displayQuestions();
+});
 
 var questionIndex = 0;
 var score = 0;
 
-
 function displayQuestions() {
-    questionsEl.textContent = "";
+  questionsEl.textContent = "";
 
-    var question = questions[questionIndex];
-    var questionDiv = document.createElement("div");
-    var questionText = document.createElement("p");
- 
-    questionText.textContent = question.title; 
+  var question = questions[questionIndex];
+  var questionDiv = document.createElement("div");
+  var questionText = document.createElement("p");
 
-       questionDiv.appendChild(questionText)
+  questionText.textContent = question.title;
 
-    
-   for (i = 0; i < question.choices.length; i++) {
-   
-        var option = document.createElement("button");
+  questionDiv.appendChild(questionText);
 
-        option.textContent = question.choices[i];
-        option.setAttribute("class", "answer");
-        option.addEventListener("click", function(e) {
-          
-            var optionClicked = (e.target.innerHTML);
-            if(optionClicked === questions[questionIndex].answer)
-            {
-                alert("correct");
-                displayQuestions(questionIndex++);
-                score += 5;
-                localStorage.setItem("score",score);
-                console.log(score);
-                //localStorage.getItem("score").innerHTML;
-            }
-            else {
-                alert("incorrect");
-                displayQuestions(questionIndex++)
-                secondsLeft -= 15;
-        
-            
-            }      
-        }) 
+  for (i = 0; i < question.choices.length; i++) {
+    var option = document.createElement("button");
+   //make an if statement to make the index stop with 4
+    option.textContent = question.choices[i];
+    option.setAttribute("class", "answer");
+    option.addEventListener("click", function(e) {
+      var optionClicked = e.target.innerHTML;
+      if ()
+      if (optionClicked === questions[questionIndex].answer) {
+        alert("correct");
+        displayQuestions(questionIndex++);
+        score += 5;
+        localStorage.setItem("score", score);
+        console.log(score);
+        //localStorage.getItem("score").innerHTML;
+      } else {
+        alert("incorrect");
+        displayQuestions(questionIndex++);
+        secondsLeft -= 15;
+      }
 
-        questionDiv.appendChild(option);
-    }
-    questionsEl.appendChild(questionDiv);
-    }
-   
+    });
 
+    questionDiv.appendChild(option);
+  }
+  questionsEl.appendChild(questionDiv);
 }
