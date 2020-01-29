c0ee16253cb8955c97f855330b610ec56d8cb856
Cleopatra <piperschwartz@aol.com>
2020-01-29 15:48:37 -0700

    Updated Quiz

--- script.js
+++ script.js
@@ -5,32 +5,59 @@ var questionsEl = document.querySelector(".questions");
 
 var secondsLeft = 75;
 var timerInterval;
+var questionIndex = 0;
+var score = 0;
 //var highScoreEl = document.querySelector("#scoring");
+var hsElArray = [];
+
+const viewHscores = document.getElementById("viewScores");
+const nameDiv = document.getElementById("nameDiv");
 
 function setTime() {
   timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
-  }, 1000);
+  }, 1000); 
+  
 }
 
+  function stopQuiz() {
+    clearInterval(timerInterval);
 
-/*function setTime() 
-{
-    timerInterval = setInterval (function ()
-    {
-        secondsLeft--;
-        timeEl.textContent = "Time:" + secondsLeft;
+    secondsLeft = 0;
+
+    document.querySelector("Time").innerHTML = "All Done!";
+
+    questionsEl.textContent = "";
+
+    score = score * (10 / questions.length);
+
+    document.getElementById("finishPage").innerHTML = "Your final score is:" + score;
+
+    nameDiv.style.display = "block";
+
+    document.getElementById("hsInitials").value = "";
+  }
+
+  function retrieveName() {
+    if (hsElArray.length ===0) {
+      highScores = document.getElementById("hsInitials").value + " - " + score;
+    }
+    else {
+      highScores = " " + document.getElementById("hsInitials").value + " - " + score;
+    }
+
+    localStorage.setItem("score", score);
+    document.querySelector(".Time").innerHTML = "High Scores";
+    document.getElementById("answer-div").innerHTML = hsElArray;
+  
     
-      
-        if(secondsLeft === 0) {
-        }
-     finish ();
-        })
-*/
+  }
+
 
 const startBtn = document.getElementById("start");
-console.log(startBtn);
+
+// event listener for start button, to start the timer
 startBtn.addEventListener("click", function() {
   setTime();
   startBtn.style.display = "none";
@@ -41,7 +68,9 @@ startBtn.addEventListener("click", function() {
 var questionIndex = 0;
 var score = 0;
 
+//function to loop through the questions
 function displayQuestions() {
+
   questionsEl.textContent = "";
 
   var question = questions[questionIndex];
@@ -52,34 +81,44 @@ function displayQuestions() {
 
   questionDiv.appendChild(questionText);
 
+
   for (i = 0; i < question.choices.length; i++) 
-  {
+      {
     var option = document.createElement("button");
-   //make an if statement to make the index stop with 4
+
     option.textContent = question.choices[i];
     option.setAttribute("class", "answer");
+
     option.addEventListener("click", function(e) {
       var optionClicked = e.target.innerHTML;
      
       if (optionClicked === questions[questionIndex].answer) {
+       
         alert("correct");
-        displayQuestions(questionIndex++);
         score += 5;
         localStorage.setItem("score", score);
+      
         console.log(score);
         //localStorage.getItem("score").innerHTML;
-
+        displayQuestions(questionIndex++); 
       } else {
         alert("incorrect");
-        displayQuestions(questionIndex++);
-        secondsLeft -= 15;
-        
+        secondsLeft -= 15;  
+          // put stop interval or change to high score page
+      displayQuestions(questionIndex++); 
       }
-    
-    });
-    
+      
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        retrieveName();
+        
+
+      }
+
+      });
+   
     questionDiv.appendChild(option);
   }
   questionsEl.appendChild(questionDiv);
-};
 
+};
